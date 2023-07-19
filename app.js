cdad4007efb702b0081e3dcb263b8342b7a7cf14
waseemrabani <waseem_ahmad63@yahoo.com>
2023-07-19 13:33:28 +0800

    feat: added apm sample app

---
+++ app.js
@@ -0,0 +1,50 @@
+// Add this to the very top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+  serviceName: 'apm-alert',
+  secretToken: 'Ehshhshshshs43h',
+  serverUrl: 'https://c6fjhkdhaksbfksbfkjbiouo57492.apm.ap-southwest-1.aws.cloud.es.io:443',
+  environment: 'development'
+})
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  apm.captureError(createError(404));
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
